71010075d87399dcd433cacc4be2b1a68d96c58c
paiiiigggeeee <91573998+paiiiigggeeee@users.noreply.github.com>
2024-02-07 12:51:36 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample_breakfast_menu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+<? xml version= "1.0" encoding= "UTF-8" ?>
+<breakfast_menu>
+<food>
+	<name> Swedish Pancakes</name>
+	<price>$11.99</price>
+	<description>
+	Includes four authentic Swedish Panackes, Lignonberry sauce, and warm sugar.
+	</description>
+	<calories>800</calories>
+	</food>
+
+	<food>
+	<name> Eggs Benedict</name>
+	<price> $12.99</price>
+	<description>
+	Includes two English Muffins, poached eggs, and our special Hollandaise sauce.
+	</description>
+	<calories>650</calories>
+	</food>
+	</breakfast_menu>
